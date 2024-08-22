c429b59825c4c2fa7d1cd91e37898e9def30231a
mubashair <mubashair@gmail.com>
2024-08-22 11:12:03 +0500

    user detail service added for more static users auth

--- src/main/java/com/web/security/CustomSecurityConfiguration.java
+++ src/main/java/com/web/security/CustomSecurityConfiguration.java
@@ -4,6 +4,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.DefaultSecurityFilterChain;
 @Configuration
 public class CustomSecurityConfiguration {
@@ -28,5 +31,16 @@ public class CustomSecurityConfiguration {
 				
 				).formLogin(Customizer.withDefaults()).build();
 		
+	}
+	//creating multiple static users with the custom class spring security configurations
+	@Bean
+	InMemoryUserDetailsManager userDetailService() {
+		UserDetails user1 = User.withDefaultPasswordEncoder().username("java").password("world").build();
+		UserDetails user2 = User.withDefaultPasswordEncoder().username("mub").password("shah").build();
+		UserDetails user3 = User.withDefaultPasswordEncoder().username("kau").password("ali").build();
+		//InMemoryUserDetailsManager userDetailsManager = new InMemoryUserDetailsManager(user1);
+		
+		//return userDetailsManager;
+		return new InMemoryUserDetailsManager(user1, user2, user3);
 	}
 }
